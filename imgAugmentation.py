e0a86ed42df7a9dbc9c5b4efd05742c78976b659
gan-xiao-nan <tr_xiao-nan.gan@vie.com.my>
2020-08-14 14:43:09 +0800

    'willBeLessbutCan'

--- imgAugmentation.py
+++ imgAugmentation.py
@@ -3,6 +3,7 @@ import fileinput
 import numpy as np
 import cv2
 from itertools import combinations
+import time
 
 def getBBlist(imgpath):
     pre,ext = os.path.splitext(imgpath)
@@ -28,16 +29,17 @@ def getBBlist(imgpath):
     return bbList
 
 def nCrList(bbList, k):
+    print('building combination ...\n')
+    start = time.time()
     output = sum([list(map(list, combinations(bbList, i))) for i in range(len(bbList) + 1)], [])
-    myCombination = []
-    for x in range(len(output)):
-        if len(output[x]) is k:
-            myCombination.append(output[x])
-        else:
-            pass
+    myCombination = [output[x] for x in range(len(output)) if len(output[x]) == k]
+    end = time.time()-start
+    print('time of find combination = ',end)
     return myCombination
 
 def findBigBB(myCombination,k):
+    print('finding big bounding box')
+    start = time.time()
     bigBB = []
     #find min_x for each combination, store in a list
     for x in range(len(myCombination)):
@@ -55,24 +57,45 @@ def findBigBB(myCombination,k):
             if myCombination[x][y][3] >= max_y:
                 max_y = myCombination[x][y][3]
         bigBB.append([min_x,min_y,max_x,max_y])
+    end = time.time()-start
+    print('time of find bigBB = ',end)
     return bigBB
 
-def dropOverlap(bigBB,bbList,k):
-    count = 0
+def isOverlap(small,big):
+    [x_min,y_min,x_max,y_max] = small
+    [x_min_bb,y_min_bb,x_max_bb,y_max_bb] = big
+    if x_min>=x_min_bb and x_max<=x_max_bb:
+        if y_min>=y_min_bb and y_max<=y_max_bb:
+            return True
+        else:
+            return False
+
+    elif(((x_min<=x_min_bb) and (x_max>=x_max_bb)) | ((y_min<=y_min_bb) and (y_max>=y_max_bb))):
+        return True
+    else:
+        return False
+
+def dropBB(bigBB,bbList,k):
+    print('dropping bounding box')
+    start = time.time()
+    boolOut = [[isOverlap(bbList[i],bigBB[j]) for i in range(len(bbList))] for j in range(len(bigBB))]
+    #final = [bigBB[i] for i in range(len(boolOut)) if sum(boolOut[i]) is k]
     final = []
-    bigBBindex = {str(i):i for i in range(len(bigBB)) }
-    for x in range(len(bigBB)):
-        for y in range (len(bbList)):
-            if (bigBB[x][0]<=bbList[y][0] and bigBB[x][1]<=bbList[y][1]):
-                if (bigBB[x][2]>=bbList[y][2] and bigBB[x][3]>=bbList[y][3]):
-                    count += 1
-            if count > k:
-                del bigBBindex[str(x)]
-                count = 0
-                break
-    for x in bigBBindex.values():
-        final.append(bigBB[x])
-    return final,bigBBindex
+    myDict = {i:i for i in range(len(bigBB))}
+    for i in range(len(boolOut)):
+        print(i)
+        if sum(boolOut[i]) is k:
+            final.append(bigBB[i])
+        else:
+            del myDict[i]
+    print('myDict','\n',myDict)  
+    print('problem set\n')
+    print('bigBB\n',bigBB[23])
+    print('isOverlap\n',boolOut[23])
+
+    end = time.time()-start
+    print('time of dropBB = ',end)
+    return final
 
 def drawBBonImage(bigBBindex,myCombination,image,imgpath,k):
     for x in bigBBindex.values():
@@ -83,35 +106,28 @@ def drawBBonImage(bigBBindex,myCombination,image,imgpath,k):
             c = int(myCombination[x][y][2])
             d = int(myCombination[x][y][3])
             cv2.rectangle(image, (a,b), (c,d), (0,255,0),2)
-        cv2.imshow('t',image) 
-        cv2.waitKey(1000)   
+        # cv2.imshow('t',image) 
+        # cv2.waitKey(1000)   
         image = original
 
-imgpath = r'C:\Users\xiao-nan.gan\internProject\padTraining\images\train\4_1_1_1.jpg'
+imgpath = r'C:\Users\xiao-nan.gan\internProject\padTraining\images\train\49043Bottom_6_3_7.jpg'
 image = cv2.imread(imgpath)
-original = image.copy()
 bbList = getBBlist(imgpath)
-
-#print(bbList,len(bbList))
-k = 3
+k = 4
 myCombination = nCrList(bbList,k)
 bigBB = findBigBB(myCombination,k)
-bigBB,bigBBindex = dropOverlap(bigBB,bbList,k)
+final = dropBB(bigBB,bbList,k)
 outputFolderPath = r'C:\Users\xiao-nan.gan\internProject\padTraining\images\imgaug'
-drawBBonImage(bigBBindex,myCombination,image,imgpath,k)
-for x in range(len(bigBB)):
-    cropped = image[bigBB[x][1]:bigBB[x][3],bigBB[x][0]:bigBB[x][2]]
+# # drawBBonImage(bigBBindex,myCombination,image,imgpath,k)
+for x in range(len(final)):
+    [x_min_bb,y_min_bb,x_max_bb,y_max_bb] = final[x]
+    cropped = image[y_min_bb:y_max_bb,x_min_bb:x_max_bb]    
     filename = 'cropped'+str(x)+'.jpg'
     outputFileName = os.path.join(outputFolderPath,filename)
     cv2.imwrite(outputFileName,cropped)
 
+cv2.rectangle(image, (1,1), (23,899), (0,255,0),2)
+image = cv2.resize(image, (416,416), interpolation = cv2.INTER_AREA)
+cv2.imshow('i',image)
 
-# for x in bigBB:
-#     a = int(x[0])
-#     b = int(x[1])
-#     c = int(x[2])
-#     d = int(x[3])
-#     print(a,b,c,d)
-#     cv2.rectangle(image, (a,b), (c,d), (0,255,0),2)
-# cv2.imshow('image',image)
-# cv2.waitKey(0)
+cv2.waitKey(0)
